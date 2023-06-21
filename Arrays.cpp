ac89bd3b65b4fd7f431271152e75bb87b7155e7c
saksham garg <sakshamgarggarg83@gmail.com>
2023-06-21 19:34:35 +0530

    insertion sort comt

--- Arrays.cpp
+++ Arrays.cpp
@@ -171,13 +171,7 @@
 //     return 0;
 // }
 
-<<<<<<< HEAD
 // searching in arrays
-=======
-<<<<<<< HEAD
-// searching in arrays
-=======
-// // searching in arrays
 // #include <iostream>
 // using namespace std;
 // int main(int argc, char const *argv[])
@@ -227,42 +221,211 @@
 //     return 0;
 // }
 
+// #include <iostream>
+// using namespace std;
+// int bs(int arr[] ,int n ,int key){
+//     int s = 0;
+//     int e = n;
+//     while(s<= e)
+//     {
+//         int mid = (s + e)/ 2;
+//         if (key == arr[mid])
+//         {
+//             return mid;
+//         }
+//         else if (key < arr[mid])
+//         {
+//             e = mid - 1;
+//         }
+//         else
+//         {
+//             s = mid + 1;
+//         }
+//     }
+//     return -1;
+// }
+// int main(int argc, char const *argv[])
+// {
+//     int n, key;
+//     cin >> n >> key;
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+//     cout<<bs(arr,n,key)<<endl;
+//     return 0;
+// }
 
-#include <iostream>
-using namespace std;
-int main(int argc, char const *argv[])
-{
-    int    
-    return 0;
-    int n, key;
-    cin >> n >> key;
-    int arr[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-    int s = 0;
-    int e = n;
-    while(s<= e)
-    {
-        int mid = (s + e / 2);
-        if (key == arr[mid])
-        {
-            cout << mid;
-        }
-        else if (key < arr[mid])
-        {
-            e = mid - 1;
-            // s++;
-        }
-        else
-        {
-            s = mid + 1;
-            // n--;
-        }
-    }
-    if (s>=e){
-        cout<<-1;
-    }
-    return 0;
-}
+// #include <iostream>
+// using namespace std;
+// int main(int argc, char const *argv[])
+// {
+//     int n, key;
+//     cin >> n >> key;
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+//      int s = 0;
+//     int e = n;
+//     while(s<= e)
+//     {
+//         int mid = (s + e)/ 2;
+//         if (key == arr[mid])
+//         {
+//             cout<< mid;
+//             break;
+//         }
+//         else if (key < arr[mid])
+//         {
+//             e = mid - 1;
+//         }
+//         else
+//         {
+//             s = mid + 1;
+//         }
+//     }
+//     if (s>e){
+//         cout<<-1;
+//     }
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main(int argc, char const *argv[])
+// {
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+//     for (int i=0;i<n;i++){
+//         for (int j=i;j<n;j++){
+//             if (arr[i]>arr[j]){
+//                 int s=arr[i];
+//                 int t=arr[j];
+//                 arr[j]=s;
+//                 arr[i]=t;
+
+//                 // int temp =arr[i];
+//                 // arr[i]=arr[j];
+//                 // arr[j]=temp;
+//             }
+//         }
+//     }
+//     for (int k=0;k<n;k++){
+//         cout<<arr[k]<<" ";
+//     }
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// void selectionSort(int arr[], int n)
+// {
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         int minIndex = i;
+//         for (int j = i + 1; j < n; j++)
+//         {
+//             if (arr[j] < arr[minIndex])
+//             {
+//                 minIndex = j;
+//             }
+//         }
+
+//         // Swap the minimum element with the current element
+//         int temp = arr[i];
+//         arr[i] = arr[minIndex];
+//         arr[minIndex] = temp;
+//     }
+// }
+
+// int main()
+// {
+//     int n;
+//     // cout << "Enter the number of elements: ";
+//     cin >> n;
+
+//     int arr[n];
+//     // cout << "Enter the elements:\n";
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+
+//     // Call the selectionSort function
+//     selectionSort(arr, n);
+
+//     // cout << "Sorted array in ascending order:\n";
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main(int argc, char const *argv[])
+// {
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+//     for (int k = 0; k < n; k++)
+//     {
+//         for (int i = 0; i < n-k; i++)
+//         {
+//             if (arr[i] > arr[i + 1])
+//             {
+//                 int temp = arr[i];
+//                 arr[i] = arr[i + 1];
+//                 arr[i + 1] = temp;
+//             }
+//         }
+//     }
+//     for (int k = 0; k < n; k++)
+//     {
+//         cout << arr[k] << " ";
+//     }
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main(int argc, char const *argv[])
+// {
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+//     for (int i=1;i<n;i++){
+//         int temp=arr[i];
+//         int j=i-1;
+//         while(j>=0 && arr[j]>temp){
+//             arr[j+1]=arr[j];
+//             j--;
+//         }
+//         arr[j+1]=temp;
+//     }
+
+//     for (int k=0;k<n;k++){
+//         cout<<arr[k]<<" ";
+//     }
+//     return 0;
+// }  
