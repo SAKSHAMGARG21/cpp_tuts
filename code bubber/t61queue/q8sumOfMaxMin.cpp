65e88f36d505ce3f2d04af74fe9743ad88002f56
SAKSHAM GARG <sakshamgarggarg83@gmail.com>
2024-08-01 11:59:48 +0530

    queue done

--- code bubber/t61queue/q8sumOfMaxMin.cpp
+++ code bubber/t61queue/q8sumOfMaxMin.cpp
@@ -55,11 +55,52 @@ int solve(int arr[], int n, int k)
 
     return ans;
 }
+
+// bs
+int solve1(int arr[], int n, int k)
+{
+
+    queue<int> q;
+    int mx = INT_MIN, mn = INT_MAX;
+    int sum=0;
+    for (int i = 0; i < k; i++)
+    {
+        q.push(arr[i]);
+    }
+
+    while (!q.empty())
+    {
+        mx = max(mx, q.front());
+        mn = min(mn, q.front());
+        q.pop();
+    }
+
+    sum+=mx+mn;
+
+    for (int i = 1; i < n-k; i++)
+    {
+        for (int j = i; j < k; j++)
+        {
+            q.push(arr[j]);
+        }
+
+        mx = INT_MIN, mn = INT_MAX;
+        while (!q.empty())
+        {
+            mx = max(mx, q.front());
+            mn = min(mn, q.front());
+            q.pop();
+        }
+        sum+=mx+mn;
+    }
+    return sum;
+}
 int main()
 {
     int arr[7] = {2, 5, -1, 7, -3, -1, -2};
 
     cout << solve(arr, 7, 4) << endl;
+    // cout << solve1(arr, 7, 4) << endl;
 
     return 0;
 }
